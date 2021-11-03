ada192b024c595ee1a4bd8297c58ab8226d4a26d
Vy Van Huynh <vyvan56@hotmail.com>
2021-11-03 10:49:11 -0500

    finished creating users, vets, questions and answers in seed.py

--- seed.py
+++ seed.py
@@ -13,7 +13,7 @@ model.connect_to_db(server.app)
 model.db.create_all()
 
 
-# Create 10 users and store them in a list
+# Create 10 users and store them in a list to create vets and questions
 users_ls = []
 for n in range(10):
     email = f'user{n}@test.com'  
@@ -24,7 +24,8 @@ for n in range(10):
     db_user = crud.create_user(email, password, vet_status, admin_status)
     users_ls.append(db_user)
 
-#Create vets from the 10 users created
+
+#Create vets from the 10 users created and store them in a list to create answers
 vets_ls =[]
 for user in users_ls:
     if user.vet_status == True:
@@ -38,8 +39,7 @@ for user in users_ls:
         vets_ls.append(db_vet)
 
 
-
-# Create questions and answers; store each in a list 
+# Create 10 questions and 10 according answers 
 question_content_ls = ["What do bunnies eat?", 
                     "Can dog eat chocolate?", 
                     "Can bunnies eat fruits?",
@@ -61,8 +61,10 @@ answer_content_ls = ["Bunnies eat hay, veggies and hay based pellets",
                     "Catnip is not considered harmful and the responses vary from sedation to hyper activity",
                     "Daily brushing is most ideal",
                     "Most of them prefer to be in pairs but some do fine being alone"]
+
 questions_in_db = []
-for question in question_content_ls:
+answers_in_db = []
+for question,answer in zip(question_content_ls,answer_content_ls):
     date_created = datetime.now()
     comment_count = randint(1,10)
     question_body = question
@@ -72,13 +74,11 @@ for question in question_content_ls:
     db_question = crud.create_question(date_created,comment_count,question_body,vote_count,user)
     questions_in_db.append(db_question)
 
-answers_in_db = []
-for answer in answer_content_ls:
     date_created = datetime.now()
     answer_body = answer
     vet = choice(vets_ls)
-    question = choice(questions_in_db) #not responding to the right question in order
-
+    question = db_question 
     db_answer = crud.create_answer(date_created,answer_body,vet,question)
     answers_in_db.append(db_answer)
+
 
