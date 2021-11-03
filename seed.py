cc1256941509cf2792c6bf12b210112eb5b47b9a
Vy Van Huynh <vyvan56@hotmail.com>
2021-11-02 22:02:02 -0500

    create vets from the 10 created users in seed.py

--- seed.py
+++ seed.py
@@ -23,8 +23,24 @@ for n in range(10):
     db_user = crud.create_user(email, password, vet_status, admin_status)
     users_ls.append(db_user)
 
+#Create vets from the 10 users created
+vets_ls =[]
+for user in users_ls:
+    if user.vet_status == True:
+        last_name = choice(['Koning','Smith','Lee','Mendez','Anderson','Huynh'])
+        license_type = 'DVM'
+        license_number = randint(1000,1999)
+        verification_status = choice(['active','expired','retired'])
+        user = user
 
-# Create questions and store them in a list 
+        db_vet = crud.create_vet(last_name,license_type,license_number,verification_status,user)
+        vets_ls.append(db_vet)
+
+
+
+
+
+# Create questions and answer; store each in a list 
 question_content_ls = ["What do bunnies eat?", 
                     "Can dog eat chocolate?", 
                     "Can bunnies eat fruits?",
@@ -33,9 +49,19 @@ question_content_ls = ["What do bunnies eat?",
                     "Can bunnies be potty trained?",
                     "How often should I bath my dog?",
                     "What does catnip do to cats?",
-                    "How often should I walk my dog?",
+                    "Can I brush my dog's teeth daily?",
                     "Do all bunnies need bondmate?"]
 
+answer_content_ls = ["Bunnies eat hay, veggies and hay based pellets",
+                    "Chocolate is toxic for dogs",
+                    "Bunnies can eat fruits but only in small amount as treats",
+                    "Bunnies cannot be bathed",
+                    "The average lifespan of an indoor cat is 13 to 17 year",
+                    "Bunnies can be potty trained to go in a litterbox",
+                    "Depend on different needs but usually once a month is good",
+                    "Catnip is not considered harmful and the responses vary from sedation to hyper activity",
+                    "Daily brushing is most ideal",
+                    "Most of them prefer to be in pairs but some do fine being alone"]
 questions_in_db = []
 for question in question_content_ls:
     date_created = datetime.now()
@@ -46,3 +72,14 @@ for question in question_content_ls:
 
     db_question = crud.create_question(date_created,comment_count,question_body,vote_count,user)
     questions_in_db.append(db_question)
+
+    answers_in_db = []
+
+for answer in answer_content_ls:
+    date_created = datetime.now()
+    answer_body = answer
+    vet = choice(vets_ls)
+    question = db_question #pointing to the last question
+
+    db_answer = crud.create_answer(date_created,answer_body,vet,question)
+
