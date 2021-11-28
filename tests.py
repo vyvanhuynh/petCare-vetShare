2f29a5b74b1e27506528099dcc2ad5f0659acdef
Vy Van Huynh <vyvan56@hotmail.com>
2021-11-28 12:36:13 -0600

    added flask tests with admin logged in to session and flasks tests using db

--- tests.py
+++ tests.py
@@ -30,6 +30,13 @@ class FlaskTestsBasic(TestCase):
         db.create_all()
         example_data()
 
+    def tearDown(self):
+        """Do at end of every test."""
+
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+        os.system('dropdb testdb')
 
     def test_index(self):
         """Test homepage page."""
@@ -45,13 +52,82 @@ class FlaskTestsBasic(TestCase):
                                   follow_redirects=True)
         self.assertIn(b"Ask A Question", result.data)
 
+
+class FlaskTestsLoggedIn(TestCase):
+    """Flask tests with admin logged in to session."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+        os.system('createdb testdb')
+
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'pet'
+        self.client = app.test_client()
+
+        # Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 2
+
     def tearDown(self):
         """Do at end of every test."""
 
         db.session.remove()
         db.drop_all()
         db.engine.dispose()
+        os.system('dropdb testdb')
 
+    def test_admin_page(self):
+        """Test admin page."""
+
+        result = self.client.get("/admin")
+        self.assertIn(b"All Users", result.data)
+
+
+class FlaskTestsDatabase(TestCase):
+    """Flask tests that use the database."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+        os.system('createdb testdb')
+
+        # Get the Flask test client
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'pet'
+
+        # Connect to test database
+        connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data
+        db.create_all()
+        example_data()
+
+    def tearDown(self):
+        """Do at end of every test."""
+
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+        os.system('dropdb testdb')
+
+    def test_users_list(self):
+        """Test admin page that show users list."""
+
+        result = self.client.get("/admin")
+        self.assertIn(b"All Users", result.data)
+
+    def test_user_details(self):
+        """Test user detail page."""
+
+        result = self.client.get("/admin/u2@test.com")
+        self.assertIn(b"Is Admin: True", result.data)
 
 
 if __name__ == "__main__":
